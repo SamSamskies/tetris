13c7d256f9bc74f2a55db901c65b6442ae179cca
Steven Miyakawa SAM <samprofessional@gmail.com>
2014-01-14 18:16:02 -0800

    Forgot to use should syntax

--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -10,10 +10,10 @@ describe Square do
 
     it "has cells with the correct coordinates" do
       cells = @square.instance_variable_get(:@cells)
-      cells[0].instance_variable_get(:@row) == 21
-      cells[1].instance_variable_get(:@row) == 21
-      cells[2].instance_variable_get(:@row) == 22
-      cells[3].instance_variable_get(:@row) == 22
+      cells[0].instance_variable_get(:@row).should eq 21
+      cells[1].instance_variable_get(:@row).should eq 21
+      cells[2].instance_variable_get(:@row).should eq 22
+      cells[3].instance_variable_get(:@row).should eq 22
     end
 
   end
