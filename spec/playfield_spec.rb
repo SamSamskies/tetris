3bd48647163e3de2c85a8878bd6ac1afd7cb81d7
Steven Miyakawa SAM <samprofessional@gmail.com>
2014-01-14 16:47:23 -0800

    Add test to make sure cells are initialized with correct index

--- spec/playfield_spec.rb
+++ spec/playfield_spec.rb
@@ -12,8 +12,12 @@ describe Playfield do
       @cells = @playfield.instance_variable_get(:@cells)
     end
 
-    it "should have a Playfield filled with Cell objects" do
+    it "should have a @cells array filled with Cell objects" do
       @cells.all? { |cell| cell.is_a? Cell }.should eq true
+    end
+
+    it "all of the cells in @cells should have an index corresponding to it's index in the array" do
+      @cells.each_with_index.all? { |cell, index| cell.instance_variable_get(:@index) == index }.should eq true
     end
 
   end
