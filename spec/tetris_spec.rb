3c5be5ec00d1e16bf63f26381e4537cbef6472d7
Steven Miyakawa SAM <samprofessional@gmail.com>
2014-01-14 16:30:08 -0800

    Add test to check if all cells in Playfield are Cell objects
    Also, refactored the tests to DRY up the code

--- spec/tetris_spec.rb
+++ spec/tetris_spec.rb
@@ -6,9 +6,21 @@ describe Tetris do
     @game = Tetris.new
   end
 
-  it "should have a Playfield with correct amount of cells when created" do
-    playfield = @game.instance_variable_get(:@playfield)
-    playfield.instance_variable_get(:@cells).count.should eq 200
+  context "when initialized" do
+
+    before(:each) do
+      playfield = @game.instance_variable_get(:@playfield)
+      @playfield_cells = playfield.instance_variable_get(:@cells)
+    end
+
+    it "should have a Playfield with correct amount of cells" do
+      @playfield_cells.count.should eq 200
+    end
+
+    it "should have a Playfield filled with Cell objects" do
+      @playfield_cells.all? { |cell| cell.is_a? Cell }
+    end
+
   end
 
 end
