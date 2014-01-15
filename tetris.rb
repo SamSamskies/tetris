adb3c87576b46fec63f9495e1d2e4469674281f0
Steven Miyakawa SAM <samprofessional@gmail.com>
2014-01-14 17:24:36 -0800

    Make random piece functionality test pass

--- tetris.rb
+++ tetris.rb
@@ -1,9 +1,16 @@
 require_relative 'playfield'
+require_relative 'square'
 
 class Tetris
 
+  VALID_PIECES = ["Square"]
+
   def initialize
     @playfield = Playfield.new
+  end
+
+  def random_piece
+    Object.const_get(VALID_PIECES.shuffle.first).new
   end
 
 end
