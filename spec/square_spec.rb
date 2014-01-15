4b2d8c45d11279dc7bfdbde481254822624dcb37
Steven Miyakawa SAM <samprofessional@gmail.com>
2014-01-14 18:52:15 -0800

    Write failing test for update cells of Square functionality

--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -4,13 +4,10 @@ describe Square do
 
   before(:each) do
     @square = Square.new
+    @cells = @square.instance_variable_get(:@cells)
   end
 
   context "when created" do
-
-    before(:each) do
-      @cells = @square.instance_variable_get(:@cells)
-    end
 
     it "has cells with the correct row coordinates" do
       @cells[0].instance_variable_get(:@row).should eq 21
@@ -24,6 +21,23 @@ describe Square do
       @cells[1].instance_variable_get(:@column).should eq 6
       @cells[2].instance_variable_get(:@column).should eq 5
       @cells[3].instance_variable_get(:@column).should eq 6
+    end
+
+  end
+
+  describe "#update_cells" do
+
+    it "updates the cells of the square accordingly" do
+      @square.update_cells Cell.new(21, 0), Cell.new(21, 1), Cell.new(22, 0), Cell.new(22, 1)
+      cells = @square.instance_variable_get(:@cells)
+      @cells[0].instance_variable_get(:@row).should eq 21
+      @cells[1].instance_variable_get(:@row).should eq 21
+      @cells[2].instance_variable_get(:@row).should eq 22
+      @cells[3].instance_variable_get(:@row).should eq 22
+      @cells[0].instance_variable_get(:@column).should eq 0
+      @cells[1].instance_variable_get(:@column).should eq 1
+      @cells[2].instance_variable_get(:@column).should eq 0
+      @cells[3].instance_variable_get(:@column).should eq 1
     end
 
   end
