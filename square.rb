d5f829701b9ebb1a011bf54cce6afe83c64f60ba
Steven Miyakawa SAM <samprofessional@gmail.com>
2014-01-14 18:56:12 -0800

    Make update cells of a Square tests pass

--- square.rb
+++ square.rb
@@ -4,4 +4,11 @@ class Square
     @cells = [Cell.new(21, 5), Cell.new(21, 6), Cell.new(22, 5), Cell.new(22, 6)]
   end
 
+  def update_cells(cell1, cell2, cell3, cell4)
+    @cells[0] = cell1
+    @cells[1] = cell2
+    @cells[2] = cell3
+    @cells[3] = cell4
+  end
+
 end
